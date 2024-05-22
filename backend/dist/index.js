8f51bfe1543648104eb44b980325812935e84441
enzosanchezariel <enzo.sanchez.ariel@gmail.com>
2024-05-22 18:38:45 -0300

    SERVIDOR EXPRESS Y CRUD PRODUCTO

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,38 @@
-"use strict";
-console.log('Test 3');
+import express from 'express';
+// Ejecutar server express
+const app = express();
+app.use('/', (req, res) => {
+    res.send("Hello!");
+});
+app.listen(3000, () => {
+    console.log("Server runnning");
+});
+// Definir una entity para el crud
+class Producto {
+    constructor(codProd, descProd, stock) {
+        this.codProd = codProd;
+        this.stock = stock;
+        this.descProd = descProd;
+    }
+    set setStock(newStock) {
+        this.stock = newStock;
+    }
+    get getStock() {
+        return this.stock;
+    }
+    set setDescProd(newDescProd) {
+        this.descProd = newDescProd;
+    }
+    get getDescProd() {
+        return this.descProd;
+    }
+    set setCodProd(newCodProd) {
+        this.codProd = newCodProd;
+    }
+    get getCodProd() {
+        return this.codProd;
+    }
+}
+const p1 = new Producto(0, "Coca-Cola", 850);
+p1.setStock = 300;
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,51 @@
-console.log('Test 3')
+import express from 'express'
+// Ejecutar server express
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send("Hello!")
+})
+
+app.listen(3000, () => {
+    console.log("Server runnning")
+})
+
+// Definir una entity para el crud
+class Producto{
+    codProd: number;
+    stock: number;
+    descProd: string;
+    
+    constructor(codProd: number, descProd: string, stock: number){
+        this.codProd = codProd;
+        this.stock = stock;
+        this.descProd = descProd;
+    }
+
+    set setStock(newStock: number){
+        this.stock = newStock;
+    }
+    
+    get getStock(){
+        return this.stock;
+    }
+    
+    set setDescProd(newDescProd: string){
+        this.descProd = newDescProd;
+    }
+    
+    get getDescProd(){
+        return this.descProd;
+    }
+
+    set setCodProd(newCodProd: number){
+        this.codProd = newCodProd;
+    }
+    
+    get getCodProd(){
+        return this.codProd;
+    }
+}
+const p1 = new Producto(0,"Coca-Cola",850);
+p1.setStock = 300;
